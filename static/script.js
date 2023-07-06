e024908c063a2b7efd190894ee327ca526719c54
Nell <nelljunior@hotmail>
2023-07-05 22:50:03 -0300

    update recognition , not fixed yet

--- static/script.js
+++ static/script.js
@@ -1,100 +1,104 @@
-/*
-   O algoritimo a seguir só funciona no Edge ou o Chrome, não adianta insistir no Chromium 
-   Também só ira funcionar em um site com certificacao SSH  (HTTPS) ou local (localhost)
-
-   Para que seja possivel o acesso a dispositivos moveis, para testes esta sendo usado o 
-   componente ngrok
-
-            Exemplo de execução 
-
-                        ngrok http 8000
-  
-
-    Para simulações com o SpeechRecognition do navegador  set flagEnableSimulation = true 
-
-  Nell Junior - Jul/23  
-
-*/
 const flagEnableSimulation = true;
+
 const makeCallButton = document.getElementById('makeCallButton');
 const audio = new Audio('./static/phone-calling-153844.mp3');
+let clienteFala = "";
+let flagNellyFalando = false;
+let flagAnswerPhoneCall = false; 
+let flagPhoneCallProgress = false;
+let flagAwaitPizzaOrderClerkResponse = true;
 
-var clienteFala = "";
-var flagNellyFalando = false;
-var flagAnswerPhoneCall = false; 
-var flagPhoneCallProgress = false;
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-recognition.interimResults = true;
+
+recognition.continuous = true;
+recognition.interimResults = false;
 recognition.lang = 'pt-BR';
+recognition.start();
 
-recognition.addEventListener("end", () => {
-  if (flagEnableSimulation) {
-      recognition.start();
-  }      
-});
+let finalTranscript = '';
+let partialResults = "";
 
-window.onload = () => {
-    fetch('/enable_simulation');
-};
+recognition.onresult = (event) => {
 
-setInterval(()=>{
-    fetch('/initiate_phone_call')
-        .then(response => response.json())
-        .then(data => {
-            if (data.flagAnswerPhoneCall && flagPhoneCallProgress == false) {
-                flagPhoneCallProgress = true;
-                makeCallButton.click()               
-            }            
-        });
-}, 1000);
+  setTimeout(()=>{
 
-recognition.addEventListener("result", (e) => {
-    const result = event.results[0][0].transcript;
-    const results = Array.from(e.results).map((result) => result[0]);
-    const transcripts = results.map((result) => result.transcript);
+      let interimTranscript = '';
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+        else interimTranscript += transcript;
+      }
+        
+      alert("mensagem pizzaria:", finalTranscript);
 
-    fetch('/ai_interaction', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ respostaCliente: result })
-    }).then(response => 
-        response.text()
-    ).then(data => {
-        flagNellyFalando = true 
-        falaAI = JSON.parse(data); 
-
-        setTimeout(()=>{
+      if (flagNellyFalando == false) {
+        fetch('/ai_interaction', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ respostaCliente: finalTranscript})
+        }).then(response => 
+            response.text()
+        ).then(data => {
+            falaAI = JSON.parse(data); 
+            console.log(falaAI.respostaNelli); 
+    
+            flagNellyFalando = true 
+            recognition.stop();
+            
             responsiveVoice.speak(falaAI.respostaNelli , "Brazilian Portuguese Female", {
                  onend: function() {
                     flagNellyFalando = false;
-                    recognition.start();
                  }
             })
-        }, 5000)    
+    
+        })               
+      }            
+}, 2000);
 
-    })
-
-});
+}
 
 const simulaAtendimento = () => {
-    audio.volume = 0.1; 
-    audio.play();
-    recognition.start()
+    try {
+        audio.volume = 0.1; 
+        audio.play();       
+        recognition.stop();
+    } catch (error) {}    
 
     setTimeout(()=>{
-        audio.pause();      
-        responsiveVoice.speak('Alô?! Aqui é da pizzaria Flor de Madureira.', 'Brazilian Portuguese Male')
+        audio.pause(); 
+        recognition.start();
+        flagPhoneCallProgress = true;
+        responsiveVoice.speak('Alô?! Aqui é da pizzaria Flor de Madureira.', 'Brazilian Portuguese Male');
     }, 9000)    
 };    
 
-makeCallButton.addEventListener('click', () => {
+window.onload = () => {
+    if (flagEnableSimulation) {
+        fetch('/enable_simulation');        
+
+        setInterval(()=>{
+            fetch('/initiate_phone_call')
+                .then(response => response.json())
+                .then(data => {
+                    if (data.flagAnswerPhoneCall && flagPhoneCallProgress == false) {
+                        flagPhoneCallProgress = true;
+                        makeCallButton.click()               
+                    }            
+                });
+        }, 1000);        
+    } 
+}
+
+makeCallButton.addEventListener('click', () => {  
+    makeCallButton.disabled = true;
+    
     if (flagEnableSimulation == false) {
-        window.location.href = 'tel:1-562-867-5309';      // TODO: Inserir numero valido asim que possivel 
+        window.location.href = 'tel:+55-11-99750-0734';      // TODO: Inserir numero valido assim que possivel 
     } else {
         simulaAtendimento();    
     }
 });
+
